108dbb8c538bbf51271c143fd8b26d7be494a2fb
Tcodel <altant@bu.edu>
2025-09-26 17:13:08 -0400

    adding code.ml

--- assigns/assign3/lib/assign3.ml
+++ assigns/assign3/lib/assign3.ml
@@ -28,26 +28,150 @@ let sep_on_whitespace (s : string) : string list =
 
 type registers = (int * int) list
 
-let load (_l : int list) : registers = assert false
+let load (l : int list) : registers = 
+   let rec build_registers acc i lst =    
+    match lst with
+    | [] -> List.rev acc    
+    | head :: rest ->       
+        if head = 0 then
+          build_registers acc (i + 1) rest   
+        else
+          build_registers ((i, head) :: acc) (i + 1) rest
+  in
+  build_registers [] 0 l
 
-let lookup (_rs : registers) (_i : int) : int = assert false
 
-let incr (_rs : registers) (_i : int) : registers = assert false
 
-let zero (_rs : registers) (_i : int) : registers = assert false
 
-let transfer (_rs : registers) (_i : int) (_j : int) : registers = assert false
 
-let parse_urm (_tokens : string list) : int list list = assert false
+let lookup (rs : registers) (i : int) : int =
+  let rec aux list =
+    match list with
+    | [] -> 0 (*if empty list return 0*)
+    | (register, value) :: tail -> (*otherwise pattern match the tuple registers*)
+        if register = i then value (*if the proper register is reached, return the associated value*)
+        else aux tail  (*else, continue searching*)
+  in
+  aux rs (*calls the main function*)
 
-let eval_urm (_prog : int list list) (_rs : registers) : registers = assert false
 
-let interp_urm (prog : string) (args : int list) : int =
-  prog
-  |> sep_on_whitespace
-  |> parse_urm
-  |> fun prog -> eval_urm prog (load args)
-  |> fun rs -> lookup rs 0
+
+
+
+let incr (rs : registers) (i : int) : registers =
+  let rec update_registers updated remaining =
+    match remaining with
+    | [] -> List.rev updated @ [(i, 1)]
+    | (register, value) :: tail -> (*iterate over the register values*)
+        if register = i then (*if we arrived at an increment command*)
+          List.rev updated @ ((register, value + 1) :: tail) (*increment*)
+        else if register > i then
+          List.rev updated @ [(i, 1)] @ (register, value) :: tail (*keeps track of default*)
+        else
+          update_registers ((register, value) :: updated) tail
+    in
+  update_registers [] rs
+
+
+
+
+let zero (rs : registers) (i : int) : registers =
+  let rec aux found rest =
+    match rest with
+    | [] -> List.rev found  (*returns the generated list.*)
+    | (j, v) :: tail ->
+        if j = i then
+          List.rev found @ tail    (* skips the value *)
+        else
+          aux ((j, v) :: found) tail
+  in
+  aux [] rs
+
+let exists (rs : registers) (i : int) : bool =
+  let rec aux l =
+    match l with
+    | [] -> false (*if it has not been found earlier return false*)
+    | (register, _) :: tail ->
+        if register = i (*for every register, loop until the element is found and return true if it's found*)
+          then true
+       else aux tail
+  in
+  aux rs  
+
+let transfer (rs : registers) (i : int) (j : int) : registers =
+  if not (exists rs i) then []
+  else
+    let val_i = lookup rs i in
+    if val_i = 0 then rs (*handles no transfer case*)
+    else
+      let rec aux found rest inserted =
+        match rest with
+        | [] ->
+            if inserted then List.rev found
+            else List.rev ((j, val_i) :: found)
+        | (k, v) :: tail ->
+            if k = j then aux ((k, val_i) :: found) tail true (*inserts the values of k, val_i *)
+            else if k > j && not inserted then aux ((j, val_i) :: (k, v) :: found) tail true (*this code continues to search recursively*)
+            else aux ((k, v) :: found) tail inserted (*adds the rest of the list*)
+      in
+      aux [] rs false
+
+
+
+
+
+exception Invalid_argument
+let parse_urm (tokens : string list) : int list list =
+  let rec aux acc tokens =
+    match tokens with
+    | [] -> List.rev acc 
+    | "Z" :: i :: rest -> (*handles zeroing case*)
+        let i_int = int_of_string i in
+        aux ([0; i_int] :: acc) rest
+    | "I" :: i :: rest -> (*handles increment case*)
+        let i_int = int_of_string i in
+        aux ([1; i_int] :: acc) rest
+    | "T" :: i :: j :: rest -> (*handles transfer*)
+        let i_int = int_of_string i in
+        let j_int = int_of_string j in
+        aux ([2; i_int; j_int] :: acc) rest
+    | "J" :: i :: j :: k :: rest -> (*parses jump command*)
+        let i_int = int_of_string i in
+        let j_int = int_of_string j in
+        let k_int = int_of_string k in
+        aux ([3; i_int; j_int; k_int] :: acc) rest
+    | _ -> raise Invalid_argument
+  in
+  aux [] tokens
+
+
+
+
+exception Invalid_Instruction
+let eval_urm (prog : int list list) (rs : registers) : registers =
+  let rec get_ith_instruction prog i =
+    match prog with
+    | [] -> None
+    | instr :: rest ->
+        if i = 0 then Some instr
+        else get_ith_instruction rest (i - 1) (*handles iterating over instructions.*)
+  in
+
+  let rec next pc rs =
+    match get_ith_instruction prog pc with
+    | None -> rs  (* program counter out of bounds*)
+    | Some instr ->
+      match instr with
+      | [0; i] -> next (pc + 1) (zero rs i) (*applies the proper function and updates pointer*)
+      | [1; i] -> next (pc + 1) (incr rs i)
+      | [2; i; j] -> next (pc + 1) (transfer rs i j)
+      | [3; i; j; k] ->
+          if lookup rs i = lookup rs j then next k rs
+          else next (pc + 1) rs
+      | _ -> raise Invalid_Instruction
+  in
+
+  next 0 rs
 
 (* Challenge problem: make this work for negative inputs *)
 let max_urm (i : int) (j : int) : int =
